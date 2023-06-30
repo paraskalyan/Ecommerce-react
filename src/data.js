12ef3f99b49a9926e99483d0e47058e982055eca
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-06-30 18:19:36 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,10 @@
+// const categories = {id: 1, img: "dfdf"}
+
+export const products = [{id:1, img:"https://images.unsplash.com/photo-1610143955896-950eb5ce3790?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHdvbWFuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"},
+                        {id:2, img:"https://img.freepik.com/free-photo/young-woman-beautiful-red-dress_1303-17506.jpg?size=626&ext=jpg&ga=GA1.2.1051330779.1687936627&semt=ais"},
+                        {id:3, img:"https://images.unsplash.com/photo-1602370463198-086436840055?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29tZW4lMjBqYWNrZXR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"},
+                        {id:4, img:"https://media.istockphoto.com/id/483534063/photo/winter-jacket-isolated-on-white.jpg?s=612x612&w=0&k=20&c=Eq80D-i6RBHlFPJaBYt4r2VpwGMRjs8P7xulYANNxqU="},
+                        {id:5, img:"https://media.istockphoto.com/id/1386033933/photo/vertical-portrait-of-a-young-woman-in-a-non-urban-environment-with-some-snow-on-the-ground.jpg?s=612x612&w=0&k=20&c=L5I70s6Ea560Q8Qu-jPLrOTyH25Cxiu6bdLQRl5onYo="},
+                        {id:6, img:"https://images.unsplash.com/photo-1603205319065-6ffbe8f0e5af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2tpcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"},
+                        {id:7, img:"https://images.unsplash.com/photo-1603205319065-6ffbe8f0e5af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2tpcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"},
+                        {id:8, img:"https://images.unsplash.com/photo-1603205319065-6ffbe8f0e5af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2tpcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"}]
