198cbd373486984d985a987927b576abbe585e9f
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-06-30 18:20:20 +0530

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import shirt from '../images/shirt.jpg'
 const Container = styled.div`
-    height: 80vh;
+    height: 90vh;
 `
 const Wrapper = styled.div`
     padding: 40px 60px;
@@ -11,10 +11,11 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-evenly;
+    position: relative;
 `
 const Card = styled.div`
     background-size: cover;
-    height: 80%;
+    height: 400px;
     width: 25%;
     background-color: #ffffff;
     display: flex;
@@ -22,21 +23,32 @@ const Card = styled.div`
     padding: 15px;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
+
 `
 const Text = styled.h1`
     font-size: 2rem;
+    z-index: 99;
+    color: white;
 `
 const Button = styled.button`
-    width: 25%;
-    font-size: 1.2rem;
+    width: 100px;
+    font-size: 1rem;
     padding: 10px 5px;
     font-weight: bold;
     cursor: pointer;
+    z-index: 99;
+    background-color: rgba(255,255,255)
     
 `
 const IMG = styled.img`
-    position:relative;
-    height: 100%;
+    position: absolute;
+    width: inherit;
+    filter: brightness(0.8);
+
+    &:hover{
+        filter: unset;
+    }
 `
 const categories = ['Shirt Style', 'Loungewear Love', 'Light Jackets']
 function Categories() {
@@ -45,7 +57,7 @@ function Categories() {
         <Wrapper>
             {categories.map((item)=>(
                 <Card key={item}>
-                    <IMG src={shirt}></IMG>
+                    <IMG src={shirt}/>
                     <Text>{item}</Text>
                     <Button>SHOP NOW</Button>
                 </Card>
