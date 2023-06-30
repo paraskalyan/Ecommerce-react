32b38e5bb3162fb5ce4279b78b7633f2e36193fc
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-06-30 18:20:54 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import Categories from '../components/Categories'
+import Products from '../components/Products'
+import Product from '../components/Product'
 
 function Home() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Slider/>
+      <Categories/>
+      <Products/>
+    </>
   )
 }
 
