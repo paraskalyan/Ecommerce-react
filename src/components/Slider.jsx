72d7bbacad81afccbdf6351e167dee811ed9d366
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-07-07 17:37:59 +0530

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styled from 'styled-components';
 import image from '../images/girl3.png'
 const Container = styled.div`
-    height: calc(100vh - 80px);
+    max-height: auto;
+    min-height: 93vh;
+
 `
 // const SlideContainer = styled.div`
 //     width: 300vw;
@@ -11,29 +13,43 @@ const Wrapper = styled.div`
     padding: 40px 100px;
     display: flex;
     column-gap: 200px;
+
+    @media screen and (max-width: 400px) {
+        flex-direction: column;
+        padding: 0 20px;
+        align-items: center;
+        justify-content: center;
+    }
 `
 const Image = styled.img`
+    @media screen and (max-width: 400px) {
+       width: 200px;
+    }
 `
 const Right = styled.div`
     display: flex;
     flex-direction: column;
-    row-gap: 20px;
-    margin-top: 100px;
-    
+    row-gap: 10px;
+    margin-top: 100px;    
+    @media screen and (max-width: 400px) {
+       margin: unset;
+       width: 100%;
+    }
 `
 const MainText = styled.h1`
-    font-size: 6rem;
+    font-size: 3rem;
     font-weight: 600;
     margin-bottom: 0;
 `
 const SubText = styled.h2`
-    line-height: 3rem;
+    line-height: 1.5rem;
     letter-spacing: 10px;
+    font-size: 1rem;
 `
 const Button = styled.button`
-    width: 160px;
-    font-size: 1.5rem;
-    padding: 15px 5px;
+    width: fit-content;
+    font-size: 1rem;
+    padding: 10px;
     cursor: pointer;
     background-color: transparent;
     font-weight: bold;
@@ -47,7 +63,7 @@ function Slider() {
   return (
    <Container>
     <Wrapper>
-        <Image src={image} width='800'/>
+        <Image src={image} width='370'/>
         <Right>
             <MainText>SUMMER SALE</MainText>
             <SubText>DONT COMPOROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</SubText>
